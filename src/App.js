6b456b249eeeb46ddff45a5723c7e0aa33a2b583
jainAdijain <jain.adityajpr1990@gmail.com>
2020-09-16 23:43:42 +0530

    feat(covidDashboard)- - added Layout component, - added Auxiliary component, - wrapped Auxiliary component with adjacent components, - added Dashboard component, - added aios package

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import Dashboard from './containers/Dashboard/Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+export class App extends Component {
+
+
+  componentDidMount() {
+
+    axios.get('https://api.covid19api.com').then(
+      response => {
+        console.log('res--', response);
+      }
+    )
+    axios.get('https://api.covid19api.com/countries').
+    then( response => {
+      console.log('res', response);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Dashboard />
+        </Layout>
+      </div>
+    )
+  }
+
 }
+
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React1
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import  React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <Aux>
+        <div>Covid Cases1</div>
+        <div>Covid Cases2</div>
+      </Aux>
+    );
+  }
+}
+
+
+export default Dashboard;
